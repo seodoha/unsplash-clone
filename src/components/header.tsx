4ca237fa5955adccfd698734a60c87b6c4b33a69
서도하\user <doha.seo89@gmail.com>
2023-11-20 22:20:18 +0900

    - eslint, prettier 세팅 - 폴더 구조 세팅

---
+++ src/components/header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className='header__logo'>
+        <Link href='/' title='윌로그'>
+          <Image
+            src='/logo_black.png'
+            width={103}
+            height={30}
+            alt='윌로그 로고'
+          />
+        </Link>
+      </h1>
+      <div className='header__util'>
+        <button className='btn ty01' title='사진 제출'>사진 제출</button>
+        <Link href='/bookmark' className='btn ty02' title='북마크 화면 바로가기'>북마크</Link>
+        <strong className='header__util__user'>
+        <span className='header__util__user_nick'>Evie</span>
+        <span className='header__util__user_email'>evie@wilog.io</span>
+      </strong>
+      </div>
+    </header>
+  )
+}
